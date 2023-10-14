34f27ae4a9aa6a6d3142ae401ec8ce74562e8cdc
mhatanorks <masaki.hatano@rakus-partners.co.jp>
2023-10-14 16:58:23 +0900

    微修正

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,19 +6,18 @@ const about = () => {
     <div className={styles.card}>
       <Image
         src={`/author.png`}
-        width={400}
-        height={400}
+        width={360}
+        height={360}
         alt="Picture of the author"
         className={styles.img}
         style={{
-          height: "auto",
           borderRadius: "99px",
           viewTransitionName: "author-image",
         }}
       />
       <div className={styles.content}>
         <p>View Transition APIのアウトプットサイトです。</p>
-        <p>View Transition APIはWeb APIの一種で、簡単に画面遷移時に<br/>アニメーションを付与する事ができます。</p>
+        <p>View Transition APIはWeb APIの一種で、簡単に画面遷移時にアニメーションを付与する事ができます。</p>
         <ul>
           <li style={{viewTransitionName: "fade-in",}}>要素ごとにアニメーションの変更が可能</li>
           <li style={{viewTransitionName: "fade-in-2",}}>遷移前後の要素を繋げたアニメーションも実装可能</li>
